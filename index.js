39cf03884b04c850628f0ce37a6f803900deee94
Clipet-Leo <leoclipet26@gmail.com>
2022-12-06 20:01:53 +0100

    initialize repository

--- index.js
+++ index.js
@@ -27,42 +27,115 @@ app.get("/pokemon/list", function (req, res) {
     //on se connecte à la DB MongoDB
     const dbConnect = dbo.getDb();
     //premier test permettant de récupérer mes pokemons !
-    dbConnect
-      .collection("Pokemon")
-      .find({}) // permet de filtrer les résultats
+    const pokemon = dbConnect.collection("Pokemon")
+    const types = dbConnect.collection("types")
+      pokemon.find({}) // permet de filtrer les résultats
       /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
       .toArray(function (err, result) {
         if (err) {
           res.status(400).send("Error fetching pokemons!");
         } else {
           res.json(result);
-        }
-      });
+        }});
   });
   
 app.post('/pokemon/insert', jsonParser, (req, res) => {
   const body = req.body;
   console.log('Got body:', body);
-  //on code ensuite l'insertion dans mongoDB, lisez la doc hehe !!
   const dbConnect = dbo.getDb();
-  dbConnect.collection('Pokemon').insertOne({'name':'reptincele'});
-  res.json(body);
+  const pokemon = dbConnect.collection('Pokemon')
+  pokemon.insertOne(body)
+    .then(function (err, result){
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.json(result);
+      }});
 });
 
 app.post('/pokemon/update', jsonParser, (req, res) => {
   const body = req.body;
   console.log('Got body:', body);
-  //on code ensuite l'insertion dans mongoDB, lisez la doc hehe !!
   const dbConnect = dbo.getDb();
-  dbConnect.collection('Pokemon').find({'name':'reptincele'}).updateOne({'name':'dracaufeu'});
-  res.json(body);
+  const pokemon = dbConnect.collection('Pokemon');
+  const result = pokemon.updateOne({'name': body.prevname}, {$set: {'name': body.newname}});
+  result.then(function (err, result){
+    if (err) {
+      res.status(400).send(err)
+    } else {
+      res.json(result)
+    }});
 });
 
 app.post('/pokemon/delete', jsonParser, (req, res) => {
   const body = req.body;
   console.log('Got body:', body);
-  //on code ensuite l'insertion dans mongoDB, lisez la doc hehe !!
   const dbConnect = dbo.getDb();
-  dbConnect.collection('Pokemon').deleteOne({'name':'dracaufeu'});
-  res.json(body);
+  const pokemon = dbConnect.collection('Pokemon');
+  pokemon.deleteOne(body)
+  .then(function (err, result){
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.json(result);
+    }});
+});
+
+/* Pokédex */
+app.get("/pokedex/list", function (req, res) {
+  //on se connecte à la DB MongoDB
+  const dbConnect = dbo.getDb();
+  //premier test permettant de récupérer mes pokemons !
+  dbConnect
+    .collection("Pokedex")
+    .find({}) // permet de filtrer les résultats
+    /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching pokemons!");
+      } else {
+        res.json(result);
+      }});
+});
+
+app.post('/pokedex/insert', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  const pokemon = dbConnect.collection('Pokedex')
+  pokemon.insertOne(body)
+    .then(function (err, result){
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.json(result);
+      }});
+});
+
+app.post('/pokedex/delete', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  const pokemon = dbConnect.collection('Pokedex');
+  pokemon.deleteOne(body)
+  .then(function (err, result){
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.json(result);
+    }});
+});
+
+app.post('/types/insert', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  const types = dbConnect.collection('types')
+  types.insertOne(body)
+    .then(function (err, result){
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.json(result);
+      }});
 });
