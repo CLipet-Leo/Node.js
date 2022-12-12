d8c7fba1c050ad5b41fbe4f9346ceda66e4b8126
Leo <lclipet@gaming.tech>
2022-12-12 15:54:08 +0100

    api fix

--- front/src/api/pokemon.js
+++ front/src/api/pokemon.js
@@ -28,14 +28,30 @@ export const addToPokedex = async (pokemon) => {
     return pokedex
 }
 
-export const getPokedex = async () => {
+export const getPokedex = async (pokemon) => {
     const response = await fetch(
         'http://localhost:4444/pokedex/list', {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-        }
+        },
+        body: JSON.stringify(pokemon)
+    }
+    )
+    const pokedex = await response.json()
+    return pokedex
+}
+
+export const delPokedex = async (pokemon) => {
+    const response = await fetch(
+        'http://localhost:4444/pokedex/list', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(pokemon)
     }
     )
     const pokedex = await response.json()
