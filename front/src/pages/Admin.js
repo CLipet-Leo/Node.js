76718978b0ca20dfb7d980743b244cd2ccab0ccf
Leo <lclipet@gaming.tech>
2022-12-13 20:13:16 +0100

    admin fix

--- front/src/pages/Admin.js
+++ front/src/pages/Admin.js
@@ -1,24 +1,25 @@
 import Menu from "../components/nav";
 import { useState, useEffect, } from "react";
-import { addToPokemon } from "../api/pokemon";
-// import { Box,TextField,Button} from '@mui/material';
+import { getAll,addToPokemon,addToPokedex,delPokemon } from "../api/pokemon";
+import { Button} from '@mui/material';
 import { useForm } from "react-hook-form";
 
-// const [pokemons, setPokemons] = useState([]);
-//       useEffect(() => { 
-//         const pokemonsFetched = addToPokemon();
-//         pokemonsFetched
-//           .then(result => setPokemons(result))
-//           .catch(error => console.error("Erreur avec notre API :", error.message));
-//       }, []);
+
 
 function Admin(props) {
     const { register, handleSubmit } = useForm();
-
     const onSubmit = (data) => {
-      console.log(data);
-      addToPokemon(data)
+        console.log(data);
+        addToPokemon(data)
     }
+    const [pokemons, setPokedex] = useState([]);
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        const pokemonsFetched = getAll();
+        pokemonsFetched
+            .then(result => setPokedex(result))
+            .catch(error => console.error("Erreur avec notre API :", error.message));
+    }, [count]);
 
   return ( <>
     <Menu />
@@ -27,7 +28,20 @@ function Admin(props) {
         <input {...register("name")} placeholder="Pokemon name" />
         <input {...register("img")} placeholder="Img" />
         <button type="submit">Ajouter</button>
-      </form>
+    </form>
+    <div class="flex">
+    {pokemons.map((pokemon, key) => {
+        return <div key={key} className="bloc-pokemon">
+        <h2>{pokemon.name}</h2>
+        <h3>{pokemon.types}</h3>
+        <img class="avatar" src={pokemon.img} alt=""/><br />
+        {/* <img class="types" src={types.img} /> */}
+        <button class="ball" onClick={()=>addToPokedex(pokemon)}><img class="ball" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFgeicwnl_0nvjfll3VVaEME0c_SjmsPL0bgU2wiSttw&s"} alt=""/> </button>
+        <br/>
+        <Button variant="contained" color="error" onClick={()=>{delPokemon(pokemon);setCount(count+1)}}>Supprimer !</Button>
+        </div>
+    })}
+    </div>
     </>
   );
 }
